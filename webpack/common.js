ea7e36a9c36487b2364002044fcda1cf62d96801
EZEKIEL ILORI <ilorieazykiel@gmail.com>
2019-08-06 15:59:48 +0100

    chore(test): setup test for app
    - install jest, @testing-library/jest-dom, @testing-library/react
    - install @testing-library/react-hooks react-test-renderer
    - setup travis.yml file
    
    [Chores test]

--- webpack/common.js
+++ webpack/common.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   resolve: {
     modules: [path.resolve(__dirname, '../src'), 'node_modules'],
-    extensions: ['*', '.js', '.jsx', '.css'],
+    extensions: [' ', '.js', '.jsx', '.css'],
     alias: {
       '<src>': path.resolve(__dirname, '../src'),
     },
